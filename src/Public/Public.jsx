21bca7df556cb92aeb05efad2f6ea87d2a137a18
TheSagnikKing <sagniknandy27@gmail.com>
2024-04-17 17:08:07 +0530

    commit

--- src/Public/Public.jsx
+++ src/Public/Public.jsx
@@ -3,26 +3,26 @@ import './Public.css'
 import { Link } from 'react-router-dom'
 
 const Public = () => {
-  return (
-    <main className='public_main_container'>
-        <div className='public_main_content'>
-            <div>
+    return (
+        <main className='public_main_container'>
+            <div className='public_main_content'>
                 <div>
-                    <img src="./public_img1.png" alt="" />
+                    <div>
+                        <img src="./public_img1.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <h1>welcome to iqb barber</h1>
+                </div>
+                <div>
+                    <div>
+                        <Link to="/adminsignin">Admin Signin</Link>
+                        <Link to="#">Barber Signin</Link>
+                    </div>
                 </div>
             </div>
-            <div>
-                <h1>welcome to iqb barber</h1>
-            </div>
-            <div>
-                <div>
-                    <Link to="/adminsignin">Admin Signin</Link>
-                    <Link to="#">Barber Signin</Link> 
-                </div>
-            </div>
-        </div>
-    </main>
-  )
+        </main>
+    )
 }
 
 export default Public
