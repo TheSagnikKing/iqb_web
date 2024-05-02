b1ab5b977ad9484ee476dfc5f2d673b412432c89
TheSagnikKing <sagniknandy27@gmail.com>
2024-05-02 12:19:27 +0530

    sidebar

--- src/components/Admin/Sidebar/Sidebar.jsx
+++ src/components/Admin/Sidebar/Sidebar.jsx
@@ -19,26 +19,27 @@ const Sidebar = () => {
       <div className={`sidebar ${showSidebar ? "show" : "hide"}`}>
         <div>
           <p className={showSidebar ? "titleActive" : "titleInActive"}>
-            {showSidebar ? <><span>IQB</span>iqueuebarbers</> : ""}
+            {showSidebar ? <><div className='sidebar_top_salon'>
+              <div>
+                <img src="https://i.pinimg.com/originals/44/e9/b5/44e9b5cb7c7d37857da5bb5685cf12cb.png" alt="" />
+              </div>
+              <p>IQB</p>
+            </div></> : ""}
           </p>
         </div>
 
-        <p className={showSidebar ? "titleActive" : "titleInActive"}>OVERVIEW</p>
-
         <div className='menu_items_container'>
           {menudata.map((m) => (
-            <div className='menu_item' key={m.id} onClick={() => navigate(m?.url)}
-              style={{
-                background: m.url === location?.pathname && " var(--hover-bg-color-1)"
-              }}
+            <div className={`menu_item ${m.url === location?.pathname && "menu_item_active"}`} key={m.id} onClick={() => navigate(m?.url)}
+
             >
               <p style={{
-                color: m.url === location?.pathname && " var(--primary-text-light-color1)"
+                color: m.url === location?.pathname && " var(--primary-bg-color3)"
               }}
               >{m.icon}</p>
               <p style={{
-                color: m.url === location?.pathname && " var(--primary-text-light-color1)"
-              }} className={showSidebar ? "titleActive" : "titleInActive"}>{m.title}</p>
+                color: m.url === location?.pathname && " var(--primary-bg-color3)"
+              }}>{m.title}</p>
             </div>
           ))}
         </div>
@@ -47,7 +48,7 @@ const Sidebar = () => {
       </div>
       <div className='content'>
         <div>
-          <Header/>
+          <Header />
           <div><Outlet /></div>
         </div>
       </div>
