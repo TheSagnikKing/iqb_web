7427277523b1edff14d655f407ecee06077075c4
TheSagnikKing <sagniknandy27@gmail.com>
2024-05-18 12:12:39 +0530

    commit

--- src/Admin/Salon/CreateSalon/CreateSalon.jsx
+++ src/Admin/Salon/CreateSalon/CreateSalon.jsx
@@ -215,6 +215,151 @@ const CreateSalon = () => {
   }, []);
 
 
+  const [startTime, setStartTime] = useState("")
+  const [startTimeDrop, setStartTimeDrop] = useState(false)
+
+  const startTimeDropHandler = () => {
+    setStartTimeDrop((prev) => !prev)
+  }
+
+  const setStartTimeHandler = (value) => {
+    setStartTime(value)
+    setStartTimeDrop(false)
+  }
+
+  const startTimeinputRef = useRef()
+  const startTimeDropRef = useRef()
+
+  useEffect(() => {
+    const handleClickStartTimeOutside = (event) => {
+      if (
+        startTimeinputRef.current &&
+        startTimeDropRef.current &&
+        !startTimeinputRef.current.contains(event.target) &&
+        !startTimeDropRef.current.contains(event.target)
+      ) {
+        setStartTimeDrop(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickStartTimeOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickStartTimeOutside);
+    };
+  }, []);
+
+
+  const [endTime, setEndTime] = useState("")
+  const [endTimeDrop, setEndTimeDrop] = useState(false)
+
+  const endTimeDropHandler = () => {
+    setEndTimeDrop((prev) => !prev)
+  }
+
+  const setEndTimeHandler = (value) => {
+    setEndTime(value)
+    setEndTimeDrop(false)
+  }
+
+  const endTimeinputRef = useRef()
+  const endTimeDropRef = useRef()
+
+  useEffect(() => {
+    const handleClickEndTimeOutside = (event) => {
+      if (
+        endTimeinputRef.current &&
+        endTimeDropRef.current &&
+        !endTimeinputRef.current.contains(event.target) &&
+        !endTimeDropRef.current.contains(event.target)
+      ) {
+        setEndTimeDrop(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickEndTimeOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickEndTimeOutside);
+    };
+  }, []);
+
+  const [timeOptions, setTimeOptions] = useState([]);
+
+  // Function to add leading zero for single-digit hours and minutes
+  const addLeadingZero = (num) => (num < 10 ? '0' : '') + num;
+
+  // Function to generate time options
+  const generateTimeOptions = () => {
+    const options = [];
+
+    // Loop through hours (0 to 23)
+    for (let hour = 0; hour < 24; hour++) {
+      // Loop through minutes (0 and 30)
+      for (let minute = 0; minute < 60; minute += 30) {
+        // Format the time as HH:mm
+        const time = addLeadingZero(hour) + ':' + addLeadingZero(minute);
+        options.push({ value: time, label: time });
+      }
+    }
+
+    setTimeOptions(options);
+  };
+
+  // Call the function to generate time options when the component mounts
+
+  useEffect(() => {
+    generateTimeOptions();
+  }, []);
+
+  console.log(timeOptions)
+
+  const [intervalTime, setIntervalTime] = useState("")
+  const [intervalTimeDrop, setIntervalTimeDrop] = useState(false)
+
+  const intervalTimeDropHandler = () => {
+    setIntervalTimeDrop((prev) => !prev)
+  }
+
+  const setIntervalTimeHandler = (value) => {
+    setIntervalTime(value)
+    setIntervalTimeDrop(false)
+  }
+
+  const intervalTimeinputRef = useRef()
+  const intervalTimeDropRef = useRef()
+
+  useEffect(() => {
+    const handleClickIntervalTimeOutside = (event) => {
+      if (
+        intervalTimeinputRef.current &&
+        intervalTimeDropRef.current &&
+        !intervalTimeinputRef.current.contains(event.target) &&
+        !intervalTimeDropRef.current.contains(event.target)
+      ) {
+        setIntervalTimeDrop(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickIntervalTimeOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickIntervalTimeOutside);
+    };
+  }, []);
+
+  const [intervalTimemin, setIntervalTimemin] = useState([])
+
+  const generateTimeIntervalInMinutes = () => {
+    const options = []
+    for (let i = 1; i <= 60; i++) {
+      options.push(i);
+    }
+
+    setIntervalTimemin(options)
+  }
+
+  useEffect(() => {
+    generateTimeIntervalInMinutes()
+  }, [])
+
   const salonTypeIconRef = useRef()
   const salonTypeDropRef = useRef()
 
@@ -513,17 +658,59 @@ const CreateSalon = () => {
           <div>
             <div>
               <p>Start Time</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={`${startTime ? `${startTime} hr` : ''}`}
+                onChange={(e) => setStartTime(e.target.value)}
+                onClick={() => startTimeDropHandler()}
+                ref={startTimeinputRef}
+              />
+
+              {startTimeDrop && <div ref={startTimeDropRef}>
+                {timeOptions.map((option) => (
+                  <p key={option} value={option} onClick={() => setStartTimeHandler(option?.value)}>
+                    {option?.value} hr
+                  </p>
+                ))}
+              </div>}
             </div>
 
             <div>
               <p>End Time</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={`${endTime ? `${endTime} hr` : ''}`}
+                onChange={(e) => setEndTime(e.target.value)}
+                onClick={() => endTimeDropHandler()}
+                ref={endTimeinputRef}
+              />
+
+              {endTimeDrop && <div ref={endTimeDropRef}>
+                {timeOptions.map((option) => (
+                  <p key={option} value={option} onClick={() => setEndTimeHandler(option?.value)}>
+                    {option?.value} hr
+                  </p>
+                ))}
+              </div>}
             </div>
 
             <div>
               <p>Intvl Tm</p>
-              <input type="text" />
+              <input
+                type="text"
+                value={`${intervalTime ? `${intervalTime} mins` : ''}`}
+                onChange={(e) => setIntervalTime(e.target.value)}
+                onClick={() => intervalTimeDropHandler()}
+                ref={intervalTimeinputRef}
+              />
+
+              {intervalTimeDrop && <div ref={intervalTimeDropRef}>
+                {intervalTimemin.map((option) => (
+                  <p key={option} value={option} onClick={() => setIntervalTimeHandler(option)}>
+                    {option} mins
+                  </p>
+                ))}
+              </div>}
             </div>
           </div>
 
@@ -678,7 +865,7 @@ const CreateSalon = () => {
               <p>Regular</p>
               <p>$20</p>
               <p>30min</p>
-              <div><DeleteIcon/></div>
+              <div><DeleteIcon /></div>
             </div>
             <div className='service_container_item'>
               <div><img src={selectedLogo ? selectedLogo : ""} alt="" /></div>
@@ -687,7 +874,7 @@ const CreateSalon = () => {
               <p>Regular</p>
               <p>$20</p>
               <p>30min</p>
-              <div><DeleteIcon/></div>
+              <div><DeleteIcon /></div>
             </div>
             <div className='service_container_item'>
               <div><img src={selectedLogo ? selectedLogo : ""} alt="" /></div>
@@ -696,7 +883,7 @@ const CreateSalon = () => {
               <p>Vip</p>
               <p>$20</p>
               <p>30min</p>
-              <div><DeleteIcon/></div>
+              <div><DeleteIcon /></div>
             </div>
             <div className='service_container_item'>
               <div><img src={selectedLogo ? selectedLogo : ""} alt="" /></div>
@@ -705,7 +892,7 @@ const CreateSalon = () => {
               <p>Regular</p>
               <p>$20</p>
               <p>30min</p>
-              <div><DeleteIcon/></div>
+              <div><DeleteIcon /></div>
             </div>
             <div className='service_container_item'>
               <div><img src={selectedLogo ? selectedLogo : ""} alt="" /></div>
@@ -714,7 +901,7 @@ const CreateSalon = () => {
               <p>Vip</p>
               <p>$20</p>
               <p>30min</p>
-              <div><DeleteIcon/></div>
+              <div><DeleteIcon /></div>
             </div>
             <div className='service_container_item'>
               <div><img src={selectedLogo ? selectedLogo : ""} alt="" /></div>
@@ -723,7 +910,7 @@ const CreateSalon = () => {
               <p>Vip</p>
               <p>$20</p>
               <p>30min</p>
-              <div><DeleteIcon/></div>
+              <div><DeleteIcon /></div>
             </div>
           </div>
 
