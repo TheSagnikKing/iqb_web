18959edd7160e9e9589ac0ab4b715c5bf2046e84
TheSagnikKing <sagniknandy27@gmail.com>
2024-06-12 18:28:46 +0530

    commit

--- src/components/Admin/MobileSidebar/MobileSidebar.jsx
+++ src/components/Admin/MobileSidebar/MobileSidebar.jsx
@@ -1,22 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './MobileSidebar.css'
-import { menudata } from '../menudata.jsx'
-import { Link, Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { Adminqueueicon, LeftArrow, RightArrow } from '../../../icons'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../Header/Header.jsx'
 import DashboardHeader from '../DashboardHeader/DashboardHeader.jsx'
-import Skeleton from 'react-loading-skeleton'
 import { useSelector } from 'react-redux'
 import { darkmodeSelector } from '../../../Redux/Admin/Reducers/AdminHeaderReducer.js'
 
 const MobileSidebar = () => {
 
-  const [showSidebar, setShowSidebar] = useState(true)
-
-  const navigate = useNavigate()
   const location = useLocation()
-
-  const [loading, setLoading] = useState(false)
 
   const darkMode = useSelector(darkmodeSelector)
 
@@ -24,45 +16,6 @@ const MobileSidebar = () => {
 
   return (
     <main className='container'>
-      {/* <div className={`sidebar ${showSidebar ? "show" : "hide"}`}>
-        <div>
-          <p className={showSidebar ? "titleActive" : "titleInActive"}>
-            {showSidebar ? <div className='sidebar_top_salon'>
-              {
-                loading ?
-                  <Skeleton count={1} height={"5rem"} width={"5rem"} style={{ borderRadius: "50%" }} /> :
-                  <>
-                    <div>
-                      <img src="https://i.pinimg.com/originals/44/e9/b5/44e9b5cb7c7d37857da5bb5685cf12cb.png" alt="" />
-                    </div>
-                    <p>IQB</p>
-                  </>
-              }
-
-            </div> : ""}
-          </p>
-        </div>
-
-        <div className='menu_items_container'>
-          {menudata.map((m) => (
-            <div className={`menu_item ${m.url === location?.pathname && "menu_item_active"}`} key={m.id} onClick={() => navigate(m?.url)}
-
-            >
-              <p style={{
-                color: m.url === location?.pathname && " var(--primary-bg-color3)"
-              }}
-              >{m.icon}</p>
-              <p style={{
-                color: m.url === location?.pathname && " var(--primary-bg-color3)"
-              }}>{m.title}</p>
-            </div>
-          ))}
-
-        </div>
-
-        <button className='sidebar_toggle_btn' onClick={() => setShowSidebar((prev) => !prev)}>{showSidebar ? <LeftArrow /> : <RightArrow />}</button>
-      </div> */}
-
       <div className={`mobile_content ${darkmodeOn && "dark"}`}
         style={{
           width: "100%"
