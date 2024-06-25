ecd7e98c9469ebe4d4f7bc7eaf0028a92e58b84f
TheSagnikKing <sagniknandy27@gmail.com>
2024-06-25 11:19:03 +0530

    Sidebar

--- src/Admin/Demo/Demo.jsx
+++ src/Admin/Demo/Demo.jsx
@@ -1,63 +1,53 @@
-// import React, { useEffect, useState } from 'react'
-// import "./Demo.css"
-// import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from "@dnd-kit/core"
-// import { SortableContext, verticalListSortingStrategy, useSortable, arrayMove, sortableKeyboardCoordinates, horizontalListSortingStrategy } from "@dnd-kit/sortable"
-// import { CSS } from "@dnd-kit/utilities"
+// import React, { useEffect, useState } from 'react';
+// import './Demo.css';
+// import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from '@dnd-kit/core';
+// import { SortableContext, horizontalListSortingStrategy, useSortable, arrayMove, sortableKeyboardCoordinates } from '@dnd-kit/sortable';
+// import { CSS } from '@dnd-kit/utilities';
 
 // const Demo = () => {
-
 //     const [tasks, setTasks] = useState([
-//         { id: 1, title: "Add test to homepage" },
-//         { id: 2, title: "Fix Styling in about section" },
-//         { id: 3, title: "How to center a div ?" },
-//         { id: 4, title: "What is html5 ? " },
-//         { id: 5, title: "Define map function" },
-//         { id: 6, title: "How to use a flexbox ?" }
-//     ])
+//         { _id: 1, title: "Add test to homepage" },
+//         { _id: 2, title: "Fix Styling in about section" },
+//         { _id: 3, title: "How to center a div ?" },
+//         { _id: 4, title: "What is html5 ?" },
+//         { _id: 5, title: "Define map function" },
+//         { _id: 6, title: "How to use a flexbox ?" },
+//         { _id: 7, title: "Add test to homepage" },
+//         { _id: 8, title: "Fix Styling in about section" },
+//         { _id: 9, title: "How to center a div ?" },
+//         { _id: 10, title: "What is html5 ?" },
+//         { _id: 11, title: "Define map function" },
+//         { _id: 12, title: "How to use a flexbox ?" }
+//     ]);
 
 //     const addTasks = (title) => {
 //         setTasks((tasks) => [...tasks, {
-//             id: tasks.length + 1,
+//             _id: tasks.length + 1,
 //             title
-//         }])
-//     }
+//         }]);
+//     };
 
-//     const [originalPos, setOriginalPos] = useState("")
-//     const [newPos, setNewPos] = useState("")
-
-//     const getTaskPos = (id) => tasks.findIndex(task => task.id === id)
+//     const getTaskPos = (id) => tasks.findIndex(task => task._id === id);
 
 //     const handleDragEnd = (event) => {
-//         const { active, over } = event
+//         const { active, over } = event;
 
-//         if (active.id === over.id) {
-//             return
+//         if (!over || active.id === over.id) {
+//             return;
 //         }
 
 //         setTasks((tasks) => {
-//             const originalPos = getTaskPos(active.id)
-//             const newPos = getTaskPos(over.id)
+//             const originalPos = getTaskPos(active.id);
+//             const newPos = getTaskPos(over.id);
 
-//             // console.log("Original Pos ",originalPos)
-//             // console.log("New Pos ",newPos)
-
-//             setOriginalPos(originalPos)
-//             setNewPos(newPos)
-
-//             return arrayMove(tasks, originalPos, newPos)
-//         })
-//     }
+//             return arrayMove(tasks, originalPos, newPos);
+//         });
+//     };
 
 //     useEffect(() => {
-//         console.log(tasks)
-//         console.log(originalPos)
-//         console.log(newPos)
-//         //For tasks array change i will call api to save the data in the database
-//     }, [tasks])
-
-
-
-//     //For Mobile Devices
+//         console.log(tasks);
+//         // For tasks array change I will call an API to save the data in the database
+//     }, [tasks]);
 
 //     const sensors = useSensors(
 //         useSensor(PointerSensor),
@@ -65,7 +55,7 @@
 //         useSensor(KeyboardSensor, { // First go to the element press enter and then ctrl hold + up/down arrow
 //             coordinateGetter: sortableKeyboardCoordinates
 //         })
-//     )
+//     );
 
 
 //     return (
@@ -74,36 +64,34 @@
 //             <div className='demo_adv_list_container'>
 //                 <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd} sensors={sensors}>
 //                     <div className='demo_column'>
-//                         <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+//                         <SortableContext items={tasks.map(task => task._id)} strategy={horizontalListSortingStrategy}>
 //                             {tasks.map((task) => (
-//                                 <Task id={task.id} title={task.title} key={task.id} />
+//                                 <Task key={task._id} id={task._id} title={task.title} />
 //                             ))}
 //                         </SortableContext>
 //                     </div>
 //                 </DndContext>
 //             </div>
 //         </div>
-//     )
-// }
+//     );
+// };
 
-// export default Demo
-
+// export default Demo;
 
 // const Task = ({ id, title }) => {
-//     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id })
+//     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
 //     const style = {
 //         transition,
 //         transform: CSS.Transform.toString(transform)
-//     }
+//     };
+
 //     return (
 //         <div className='demo_task' ref={setNodeRef} {...attributes} {...listeners} style={style}>
-//             <input type="checkbox" className='demo_checkbox' />
-//             {title}
+//             <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80" alt="" />
 //         </div>
-//     )
-// }
-
+//     );
+// };
 
 import React, { useEffect, useState } from 'react';
 import './Demo.css';
@@ -112,29 +100,32 @@ import { SortableContext, horizontalListSortingStrategy, useSortable, arrayMove,
 import { CSS } from '@dnd-kit/utilities';
 
 const Demo = () => {
-    const [tasks, setTasks] = useState([
-        { _id: 1, title: "Add test to homepage" },
-        { _id: 2, title: "Fix Styling in about section" },
-        { _id: 3, title: "How to center a div ?" },
-        { _id: 4, title: "What is html5 ?" },
-        { _id: 5, title: "Define map function" },
-        { _id: 6, title: "How to use a flexbox ?" },
-        { _id: 7, title: "Add test to homepage" },
-        { _id: 8, title: "Fix Styling in about section" },
-        { _id: 9, title: "How to center a div ?" },
-        { _id: 10, title: "What is html5 ?" },
-        { _id: 11, title: "Define map function" },
-        { _id: 12, title: "How to use a flexbox ?" }
+    const [advState, setAdvState] = useState([
+        {
+            "public_id": "advertisements/salon-1/rose_1719209947907_52e2ff26-db64-4bd1-8396-192f4f45a9fc",
+            "url": "https://res.cloudinary.com/dpynxkjfq/image/upload/v1719209948/advertisements/salon-1/rose_1719209947907_52e2ff26-db64-4bd1-8396-192f4f45a9fc.jpg",
+            "_id": "66790fddc69b5b49cf8854df"
+        },
+        {
+            "public_id": "advertisements/salon-1/kbimage_1719209947906_19cfd58a-c53c-42e6-9f34-113aca480ca9",
+            "url": "https://res.cloudinary.com/dpynxkjfq/image/upload/v1719209948/advertisements/salon-1/kbimage_1719209947906_19cfd58a-c53c-42e6-9f34-113aca480ca9.png",
+            "_id": "66790fddc69b5b49cf8854de"
+        },
+        {
+            "public_id": "advertisements/salon-1/gokukb_1719209947897_033189bd-5a2c-4da4-a404-bbadedf9b0e4",
+            "url": "https://res.cloudinary.com/dpynxkjfq/image/upload/v1719209948/advertisements/salon-1/gokukb_1719209947897_033189bd-5a2c-4da4-a404-bbadedf9b0e4.jpg",
+            "_id": "66790fddc69b5b49cf8854dd"
+        }
     ]);
 
-    const addTasks = (title) => {
-        setTasks((tasks) => [...tasks, {
-            _id: tasks.length + 1,
-            title
-        }]);
-    };
+    // const addTasks = (title) => {
+    //     setTasks((tasks) => [...tasks, {
+    //         _id: tasks.length + 1,
+    //         title
+    //     }]);
+    // };
 
-    const getTaskPos = (id) => tasks.findIndex(task => task._id === id);
+    const getAdvPos = (id) => advState.findIndex(adv => adv._id === id);
 
     const handleDragEnd = (event) => {
         const { active, over } = event;
@@ -143,18 +134,18 @@ const Demo = () => {
             return;
         }
 
-        setTasks((tasks) => {
-            const originalPos = getTaskPos(active.id);
-            const newPos = getTaskPos(over.id);
+        setAdvState((advs) => {
+            const originalPos = getAdvPos(active.id);
+            const newPos = getAdvPos(over.id);
 
-            return arrayMove(tasks, originalPos, newPos);
+            return arrayMove(advs, originalPos, newPos);
         });
     };
 
     useEffect(() => {
-        console.log(tasks);
+        console.log(advState);
         // For tasks array change I will call an API to save the data in the database
-    }, [tasks]);
+    }, [advState]);
 
     const sensors = useSensors(
         useSensor(PointerSensor),
@@ -163,7 +154,7 @@ const Demo = () => {
             coordinateGetter: sortableKeyboardCoordinates
         })
     );
-    
+
 
     return (
         <div className='demo_container'>
@@ -171,9 +162,9 @@ const Demo = () => {
             <div className='demo_adv_list_container'>
                 <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd} sensors={sensors}>
                     <div className='demo_column'>
-                        <SortableContext items={tasks.map(task => task._id)} strategy={horizontalListSortingStrategy}>
-                            {tasks.map((task) => (
-                                <Task key={task._id} id={task._id} title={task.title} />
+                        <SortableContext items={advState.map(adv => adv._id)} strategy={horizontalListSortingStrategy}>
+                            {advState.map((adv) => (
+                                <Adv key={adv._id} id={adv._id} url={adv.url} />
                             ))}
                         </SortableContext>
                     </div>
@@ -185,7 +176,7 @@ const Demo = () => {
 
 export default Demo;
 
-const Task = ({ id, title }) => {
+const Adv = ({ id, url }) => {
     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
     const style = {
@@ -195,7 +186,7 @@ const Task = ({ id, title }) => {
 
     return (
         <div className='demo_task' ref={setNodeRef} {...attributes} {...listeners} style={style}>
-            <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80" alt="" />
+            <img src={url} alt="" />
         </div>
     );
 };
